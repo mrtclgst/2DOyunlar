e8af3b54342292a49b6300489f7e45db1d18c6d7
mrtclgst <mrtclgst@gmail.com>
2022-04-19 13:56:33 +0300

    .

--- Assets/01SnakeGame/Scripts/SnakeGameFoodCs.cs
+++ Assets/01SnakeGame/Scripts/SnakeGameFoodCs.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class SnakeGameFoodCs : MonoBehaviour
 {
     [SerializeField] BoxCollider2D _gridArea;
-    private void Start() {
+    private void Start()
+    {
         RandomizePosition();
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-            RandomizePosition();
+    void RandomizePosition()
+    {
+        Bounds bounds = _gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+        transform.position = new Vector2(Mathf.Round(x), Mathf.Round(y));
     }
-    void RandomizePosition(){
-        Bounds bounds=_gridArea.bounds;
-        float x=Random.Range(bounds.min.x,bounds.max.x);
-        float y=Random.Range(bounds.min.y,bounds.max.y);
-        transform.position=new Vector3( Mathf.Round(x),Mathf.Round(y));
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        RandomizePosition();
     }
-
-
-
 }
