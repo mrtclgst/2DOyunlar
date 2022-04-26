4b5aa89e9d35a4c1ea6ce9d8b54c29f2b61cf6f1
mrtclgst <mrtclgst@gmail.com>
2022-04-26 12:15:06 +0300

    .

---
+++ Assets/03 DonkeyKong/Scripts/Player.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DonkeyKong
+{
+    public class Player : MonoBehaviour
+    {
+        private void Awake()
+        {
+
+        }
+    }
+}
+
