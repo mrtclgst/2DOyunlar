56148e5d5a4c3343d57a675d2a5d3d856316f2c4
mrtclgst <mrtclgst@gmail.com>
2022-04-26 14:29:56 +0300

    .

--- Assets/03 DonkeyKong/Scripts/Player.cs
+++ Assets/03 DonkeyKong/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,37 @@ namespace DonkeyKong
 {
     public class Player : MonoBehaviour
     {
+        Rigidbody2D rg;
+        bool jumped, isOnGround;
+        Vector2 direction;
+        [SerializeField] float moveSpeed, jumpForce;
         private void Awake()
+        {
+            AwakeRef();
+        }
+        private void Update()
+        {
+            CheckCollision();
+            if (Input.GetButtonDown("Jump")) direction = Vector2.up * jumpForce;
+            else direction += Physics2D.gravity * Time.deltaTime;
+            //karakterimizi aasgi cekmek icin gravity ekliyoruz.
+
+            direction.x = Input.GetAxis("Horizontal") * moveSpeed;
+            direction.y = MathF.Max(direction.y, -1f);
+            //gravity -9.81 diye gectiginden yukari gitmemek icin -1e kadar sinirli tuttuk.
+
+            if (direction.x > 0f) transform.eulerAngles = Vector3.zero;
+            else if (direction.x < 0f) transform.eulerAngles = Vector3.up * 180;
+        }
+        private void FixedUpdate()
+        {
+            rg.MovePosition(rg.position + direction * Time.fixedDeltaTime);
+        }
+        private void AwakeRef()
+        {
+            rg = GetComponent<Rigidbody2D>();
+        }
+        void CheckCollision()
         {
 
         }
